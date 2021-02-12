1d868505b5c3d6daa89ac5253b628992f57f2ea2
Guowen Yang <2512280y@student.gla.ac.uk>
2021-02-12 21:56:58 +0800

    char 8

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,23 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
-# Create your views here.
-
+from rango.forms import CategoryForm
+from rango.forms import PageForm
+from django.shortcuts import redirect
+from django.urls import reverse
 
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-	# Note the key boldmessage matches to {{ boldmessage }} in the template!
     category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
-
+    pages_list = Page.objects.order_by('-views')[:5]
+    context_dict['pages'] = pages_list
+    # context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context=context_dict)
+    # return HttpResponse("Rango says hey there partner! Go to <a href='/rango/about/'>About</a>")
+
 
 def about(request):
-    context_dict = {'boldmessage': ' This tutorial has been put together by Guowen Yang'}
+    context_dict = {'boldmessage': 'This tutorial has been put together by Inesh Bose.'}
     return render(request, 'rango/about.html', context=context_dict)
+    # return HttpResponse("Rango says here is the about page. Go to <a href='/rango/'>Index</a>")
 
 
 def show_category(request, category_name_slug):
@@ -28,14 +33,10 @@ def show_category(request, category_name_slug):
         pages = Page.objects.filter(category=category)
         context_dict['pages'] = pages
         context_dict['category'] = category
-
     except Category.DoesNotExist:
         context_dict['pages'] = None
-
         context_dict['cateogry'] = None
-
     return render(request, 'rango/category.html', context=context_dict)
-
 
 
 def add_category(request):
@@ -60,12 +61,9 @@ def add_page(request, category_name_slug):
         return redirect('/rango/')
 
     form = PageForm()
-
     if request.method == 'POST':
         form = PageForm(request.POST)
-
         if form.is_valid():
-
             if category:
                 page = form.save(commit=False)
                 page.category = category
